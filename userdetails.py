dbdc45fca4217776e1c23b3a8e06d9ffb744fb84
Rachana.v.angadi <rachanaangadi30@gmail.com>
2026-09-04 10:24:31 +0530

    saving the file to GitHub

---
+++ userdetails.py
@@ -0,0 +1,6 @@
+name = input("Enter your name:")
+age = input("Enter your age:")
+city = input("Enter your city:")
+email = input("Enter your email:")
+
+print(f"Name: {name}, Age: {age}, City: {city}, Email: {email}")
